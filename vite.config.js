190114a7530eaace2e8352dee83063a2fb487df7
rodrigoaiz <rodrigoaiz@gmail.com>
2023-12-06 19:42:31 -0600

    vite.config para build rdy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-const path = require('path')
+import { resolve } from 'path';
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  base: './',
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+  },
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      '~bootstrap-icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+      '~bootstrap-icons': resolve(__dirname, 'node_modules/bootstrap-icons'),
     }
   },
   server: {
     port: 8080,
     hot: true
   }
-}
+};
